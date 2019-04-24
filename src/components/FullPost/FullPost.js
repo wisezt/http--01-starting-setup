fbe919aca1e931b978261c2bf5b7d10545c74e76
Ting <wisezt@hotmail.com>
2019-04-24 10:36:26 -0500

    Tutorial 193 delete and catch error

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -16,10 +16,21 @@ class FullPost extends Component {
                         console.log(response);
                         this.setState({loadedPost: response.data}); // not just response but response.data
                     }
-                );
+                ).catch(error=>{
+                    console.log(error);
+                });
             }
         }
     }
+
+    deletePostHandler =()=>{
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(
+            response =>{
+                console.log(response);
+            }
+        )
+    }
+
 
     render () {
         let post = <p style={{textAlign:'center'}}>Please select a Post!</p>;
@@ -35,7 +46,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
                     </div>
                 </div>
 
