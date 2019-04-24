65242689c50d112dece94f13adb160000c62b711
Ting <wisezt@hotmail.com>
2019-04-24 10:08:35 -0500

    Tutorial 192 Fetching Data and Update (without infinite loop)

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    state={
+        loadedPost: null
+    }
+
+
+    componentDidUpdate(){
+        if (this.props.id){
+            if (!this.state.loadedPost || (this.state.loadedPost && (this.state.loadedPost.id !== this.props.id))) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id).then(response => {
+                        console.log(response);
+                        this.setState({loadedPost: response.data}); // not just response but response.data
+                    }
+                );
+            }
+        }
+    }
+
     render () {
         let post = <p style={{textAlign:'center'}}>Please select a Post!</p>;
-        if (this.props.id) {
+        if (this.props.id){
+            post =<p style={{textAlign:'center'}}> Loading...!</p>;
+        }
+
+        if (this.state.loadedPost) { // here the this.state.loadedPost will be checked if it has value even though the this.state.loadedPost is not a boolean.
 
 
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
