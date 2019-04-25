732d1f669f09baf03932bcbfc9865fa579d78707
Ting <wisezt@hotmail.com>
2019-04-24 21:03:02 -0500

    Tutorial 193 the "previous" and "next" button need to be fixed!

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -7,88 +7,98 @@ import './Blog.css';
 import axios from 'axios';
 
 class Blog extends Component {
-    state ={
+    state = {
         posts: [], // define posts as an array
         SelectedPostId: null,
         currentPages: 0,
         nextPages: 4,
         totalPages: 0,
-        allPosts:[],
+        allPosts: [],
 
         stateNumber: 0
     }
-    currentPage = 0 ;
+    currentPage = 0;
+    previousPage = 0;
+    nextPage = 0;
 
 
-    componentDidMount(){
+    componentDidMount() {
 
-        axios.get('https://jsonplaceholder.typicode.com/posts').then(response=>
-            {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
 
                 this.setState({allPosts: response.data});
                 console.log(this.state.allPosts.length)
                 const posts = response.data.slice(0, 4);
-                const updatedPosts =posts.map(post =>{
+                const updatedPosts = posts.map(post => {
                     return {
                         ...post,
                         author: 'Max'
                     }
                 })
                 this.setState({posts: updatedPosts}); // response.data is array and put the array into state.posts
-                console.log(response);}
+                console.log(response);
+            }
         )
         ;
 
 
-
-
     }
 
-    postSelectedHandler = (id) =>{
-            this.setState({selectedPostId:id});
-}
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
 
 
-    clickPreviousPages=()=>{
+    clickPreviousPages = () => {
+        this.currentPage = this.currentPage-4;
+        this.previousPage = this.previousPage- 4;
+        this.nextPage = this.nextPage - 4;
 
+        let updatedPosts = this.state.allPosts.slice(this.previousPage, this.currentPage);
 
-
-
-       let updatedPosts = this.state.allPosts.slice(this.currentPage, this.currentPage-4);
-       this.currentPage = this.currentPage -4;
         this.setState({posts: updatedPosts});
-
+        console.log('currentPage: ' + this.currentPage);
+        console.log('previousPage: ' + this.previousPage);
+        console.log('nextPage: ' + this.nextPage);
     }
 
-    clickNextPages=()=>{
-       let updatedPosts = this.state.allPosts.slice(this.currentPage, this.currentPage+4);
-        this.currentPage = this.currentPage +4;
+    clickNextPages = () => {
+        this.currentPage = this.currentPage+4;
+        this.previousPage = this.previousPage+ 4;
+        this.nextPage = this.nextPage + 4;
+
+        let updatedPosts = this.state.allPosts.slice(this.currentPage, this.nextPage);
         this.setState({posts: updatedPosts});
+        console.log('currentPage: ' + this.currentPage);
+        console.log('previousPage: ' + this.previousPage);
+        console.log('nextPage: ' + this.nextPage);
 
     }
 
-    render () {
-        const posts =this.state.posts.map(post =>{  // map the array to JSX
+    render() {
+        const posts = this.state.posts.map(post => {  // map the array to JSX
             return <Post
                 key={post.id}
                 title={post.title}
                 author={post.author}
-                 clicked={()=>this.postSelectedHandler(post.id)}
+                clicked={() => this.postSelectedHandler(post.id)}
 
-            />;});
+            />;
+        });
 
         return (
             <div>
+                <button onClick={this.clickPreviousPages}>Previous</button>
+                <button onClick={this.clickNextPages}>Next</button>
                 <section className="Posts">
                     {posts}
                 </section>
-                <button onClick={this.clickPreviousPages}>Previous</button>
-                <button onClick={this.clickNextPages}>Next</button>
+
                 <section>
-                    <FullPost id={this.state.selectedPostId} />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
-                    <NewPost />
+                    <NewPost/>
                 </section>
             </div>
         );
